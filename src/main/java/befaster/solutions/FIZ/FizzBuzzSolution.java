b2690be0994e64d06925c27683f011ff540b48bb
sbx_user1051 <sbx_user1051@169.254.28.77>
2024-03-16 19:08:26 +0000

    Sat Mar 16 19:08:26 UTC 2024

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,18 +7,43 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
        boolean isFizz = number % 3 ==0 || String.valueOf(number).contains("3");
        boolean isBuzz = number % 5 ==0 || String.valueOf(number).contains("5");
-      if(isFizz &&  isBuzz){
+       boolean isDeluxe = number > 10 && digitsAreIdentical(number);
+      if(isFizz &&  isBuzz && isDeluxe){
+          return "fizz buzz deluxe";
+      }
+      else if (isFizz && isDeluxe){
+          return "fizz deluxe";
+      }
+      else if (isBuzz && isDeluxe) {
+          return "buzz deluxe";
+      }
+      else if (isFizz && isBuzz) {
           return "fizz buzz";
       }
-      else if (isFizz){
+      else if (isFizz) {
           return "fizz";
       }
       else if (isBuzz) {
           return "buzz";
+      }
+      else if (isDeluxe) {
+          return "deluxe";
       }
       else{
           return String.valueOf(number);
       }
     }
 
+    private boolean digitsAreIdentical(int number){
+        String numStr = String.valueOf(number);
+        char firstDigit = numStr.charAt(0);
+        for(int i = 1; i<numStr.length(); i++){
+            if(numStr.charAt(i) != firstDigit){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
+
