fc8167ee8bf0382ec4a21133f6c2b0b36822153d
sbx_user1051 <sbx_user1051@169.254.28.77>
2024-03-16 19:14:24 +0000

    Sat Mar 16 19:14:24 UTC 2024

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,8 +7,10 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
        boolean isFizz = number % 3 ==0 || String.valueOf(number).contains("3");
        boolean isBuzz = number % 5 ==0 || String.valueOf(number).contains("5");
-       boolean isDeluxe = number > 10 && digitsAreIdentical(number);
-      if(isFizz &&  isBuzz && isDeluxe){
+       boolean isDeluxe = number > 10 && digitsAreIdentical(number) && number % 2 == 0;
+        boolean isFakeDeluxe = number > 10 && digitsAreIdentical(number) && number % 2 != 0;
+
+        if(isFizz &&  isBuzz && isDeluxe){
           return "fizz buzz deluxe";
       }
       else if (isFizz && isDeluxe){
@@ -29,6 +31,9 @@ public class FizzBuzzSolution {
       else if (isDeluxe) {
           return "deluxe";
       }
+      else if (isFakeDeluxe) {
+          return "fake deluxe";
+      }
       else{
           return String.valueOf(number);
       }
@@ -46,4 +51,5 @@ public class FizzBuzzSolution {
     }
 
 }
+
 
